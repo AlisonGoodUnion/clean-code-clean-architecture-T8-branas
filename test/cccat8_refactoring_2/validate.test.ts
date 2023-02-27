d3b7b9c7c67cc9d5d039b7ed9e511e47a7cce1f3
Alison Ramos da Silva <alisonal1@hotmail.com>
2023-02-27 11:03:06 -0300

    Aula2: Tecnicas de Refactoring: Criacao de testes antes do Refactoring

---
+++ test/cccat8_refactoring_2/validate.test.ts
@@ -0,0 +1,42 @@
+import {validate} from "../../src/cccat8_refactoring_2/validate";
+import expect from "expect";
+
+test("Deve validar o cpf válido que tem dígito maior de zero", function () {
+    const isValid = validate('356.232.330-15');
+    expect(isValid).toBeTruthy();
+});
+
+test("Deve validar o cpf com dígito zero no primeiro dígito", function () {
+    const isValid = validate('815.128.790-02');
+    expect(isValid).toBeTruthy();
+});
+
+test("Deve validar o cpf com dígito zero no segundo dígito", function () {
+    const isValid = validate('893.458.200-60');
+    expect(isValid).toBeTruthy();
+});
+
+test("Deve tentar validar cpf com mais de 14 characteres", function () {
+    const isValid = validate('893.458.200-600');
+    expect(isValid).toBeFalsy();
+});
+
+test("Deve tentar validar cpf undefined", function () {
+    const isValid = validate(undefined);
+    expect(isValid).toBeFalsy();
+});
+
+test("Deve tentar validar cpf null", function () {
+    const isValid = validate(null);
+    expect(isValid).toBeFalsy();
+});
+
+test("Deve tentar validar cpf com digitos iguais", function () {
+    const isValid = validate('111.111.111-11');
+    expect(isValid).toBeFalsy();
+});
+
+test("Deve tentar validar cpf com NaN", function () {
+    const isValid = validate('abc.abc.abc-ab');
+    expect(isValid).toBeFalsy();
+});
